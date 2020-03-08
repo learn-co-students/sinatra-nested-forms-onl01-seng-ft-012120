02bc4d24d3c649a3d2182a9ee52d4f8c21914bae
Beekasha <brian.j.kleiner@gmail.com>
2020-03-08 00:42:31 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+
+    @all = []
+
+    def initialize(name, height, weight)
+        @name = name
+        @height = height
+        @weight = weight
+        @all << self
+    end
 end
